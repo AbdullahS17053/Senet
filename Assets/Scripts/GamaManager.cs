0d8feff65739b45afdbd417fb515c9cfad6df727
Muhammad Hadi Naeem <hadinaeem365@gmail.com>
2025-07-15 16:21:55 +0500

    Asset Settings

--- Assets/Scripts/GamaManager.cs
+++ Assets/Scripts/GamaManager.cs
@@ -1,21 +1,76 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("UI Panels")]
     [SerializeField] private GameObject scrollPanel;
+    
+    [Header("Tap Effect Settings")]
+    [SerializeField] private float scaleAmount = 1.1f;
+    [SerializeField] private float scaleDuration = 0.1f;
+    [SerializeField] private float afterEffectDelay = 0.1f;
     public void PlayBtn()
     {
-        SceneManager.LoadScene(1);
+        StartCoroutine(PlayTapEffect(() =>
+        {
+            SceneManager.LoadScene(1);
+        }));
     }
 
     public void ScrollBtn()
     {
-        scrollPanel.SetActive(true);
+        StartCoroutine(PlayTapEffect(() =>
+        {
+            scrollPanel.SetActive(true);
+        }));
+            
     }
 
     public void BackBtn()
     {
         scrollPanel.SetActive(false);
     }
+    public IEnumerator PlayTapEffect(System.Action onComplete)
+    {
+        GameObject clicked = EventSystem.current.currentSelectedGameObject;
+        if (clicked == null)
+        {
+            onComplete?.Invoke();
+            yield break;
+        }
+
+        Transform transform = clicked.transform;
+
+        Vector3 originalScale = transform.localScale;
+        Vector3 targetScale = originalScale * scaleAmount;
+
+        float timer = 0f;
+        while (timer < scaleDuration)
+        {
+            transform.localScale = Vector3.Lerp(originalScale, targetScale, timer / scaleDuration);
+            timer += Time.unscaledDeltaTime;
+            yield return null;
+        }
+
+        transform.localScale = targetScale;
+
+        timer = 0f;
+        while (timer < scaleDuration)
+        {
+            transform.localScale = Vector3.Lerp(targetScale, originalScale, timer / scaleDuration);
+            timer += Time.unscaledDeltaTime;
+            yield return null;
+        }
+
+        transform.localScale = originalScale;
+
+        yield return new WaitForSeconds(afterEffectDelay);
+
+        onComplete?.Invoke();
+    }
 }
+
+
